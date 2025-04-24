306f3a2fbe9cacba2668ef5820fde24ebe2cf0e1
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-04-24 19:29:47 +0200

    zimletAdmin: Edit HSM Policy types

--- adminZimlet/com_btactic_hsm_admin/com_btactic_hsm_admin.js
+++ adminZimlet/com_btactic_hsm_admin/com_btactic_hsm_admin.js
@@ -79,15 +79,28 @@ if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_hsm_admin"]){
                             removeButtonLabel : com_btactic_hsm_admin.Remove_zimbraHsmPolicy,
                             removeButtonCSSStyle : "margin-left: 50px",
                             visibilityChecks : [ ZaItem.hasReadPermission ],
-                              items : [ {
-                                ref : ".",
-                                type : _TEXTFIELD_,
-                                label : null,
-                                labelLocation : _NONE_,
-                                toolTipContent : com_btactic_hsm_admin.tt_zimbraHsmPolicy,
-                                visibilityChecks : [ ZaItem.hasReadPermission ],
-                                width : "80em"
-                              } ]
+                            items: [
+                                {
+                                    ref: ".",
+                                    type: _TEXTFIELD_,
+                                    label: null,
+                                    labelLocation: _NONE_,
+                                    toolTipContent : com_btactic_hsm_admin.tt_zimbraHsmPolicy,
+                                    width: "60em",
+                                    visibilityChecks: [ ZaItem.hasReadPermission ]
+                                },
+                                {
+                                    type: _DWT_BUTTON_,
+                                    label: com_btactic_hsm_admin.EditButtonLabel,
+                                    width: "10em",
+                                    onActivate: function () {
+                                      let form = this.getForm();
+                                      let parentItem = this.getParentItem(); // gets the XFormItem
+                                      let currentHSMValue = parentItem.getInstanceValue();
+                                      com_btactic_hsm_ext.launchEditWizard(currentHSMValue, parentItem, this);
+                                    }
+                                }
+                            ]
                             }
 
                         ]
@@ -161,15 +174,28 @@ if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_hsm_admin"]){
                             removeButtonLabel : com_btactic_hsm_admin.Remove_zimbraHsmPolicy,
                             removeButtonCSSStyle : "margin-left: 50px",
                             visibilityChecks : [ ZaItem.hasReadPermission ],
-                              items : [ {
-                                ref : ".",
-                                type : _TEXTFIELD_,
-                                label : null,
-                                labelLocation : _NONE_,
-                                toolTipContent : com_btactic_hsm_admin.tt_zimbraHsmPolicy,
-                                visibilityChecks : [ ZaItem.hasReadPermission ],
-                                width : "80em"
-                              } ]
+                            items: [
+                                {
+                                    ref: ".",
+                                    type: _TEXTFIELD_,
+                                    label: null,
+                                    labelLocation: _NONE_,
+                                    toolTipContent : com_btactic_hsm_admin.tt_zimbraHsmPolicy,
+                                    width: "60em",
+                                    visibilityChecks: [ ZaItem.hasReadPermission ]
+                                },
+                                {
+                                    type: _DWT_BUTTON_,
+                                    label: com_btactic_hsm_admin.EditButtonLabel,
+                                    width: "10em",
+                                    onActivate: function () {
+                                      let form = this.getForm();
+                                      let parentItem = this.getParentItem(); // gets the XFormItem
+                                      let currentHSMValue = parentItem.getInstanceValue();
+                                      com_btactic_hsm_ext.launchEditWizard(currentHSMValue, parentItem, this);
+                                    }
+                                }
+                            ]
                             }
 
                         ]
@@ -192,5 +218,166 @@ if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_hsm_admin"]){
         }
         ZaItem.loadMethods["ZaServer"].push(ZaServer.loadHsmMethod);
     }
+
+    // Helper to launch the HSM Policy Edit Wizard
+    com_btactic_hsm_ext.CustomZaXFormDialog = function (params) {
+        shell = params.parent;
+        title = params.title;
+        if (!shell) {
+            throw new Error("Shell must be provided to CustomZaXFormDialog");
+        }
+
+        if (!(shell instanceof DwtShell)) {
+          throw new Error("shell must be a DwtShell (CustomZaXFormDialog)");
+        }
+
+        this._standardButtons = params.standardButtons;
+
+        // Call the parent constructor (ZaXDialog)
+        ZaXDialog.call(this, shell, null, title, params.w, params.h, params.iKeyName, params.contextId);
+
+        // Store the shell reference
+        this.shell = shell;
+    };
+
+    // Inherit from ZaXDialog
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype = new ZaXDialog();
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype.constructor = com_btactic_hsm_ext.CustomZaXFormDialog;
+
+    // Override setHSMEditObject to set the object to be edited
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype.setHSMEditObject = function (obj) {
+        this._HSMEditObject = obj;  // Store the object
+    };
+
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype.setContent = function (content) {
+        this._xformDef = {
+            type: _GROUP_,
+            numCols: 1,
+            items: [
+                { type: _CHECKBOX_, ref: "message", label: "E-mails" },
+                { type: _CHECKBOX_, ref: "document", label: "Documents" },
+                { type: _CHECKBOX_, ref: "task", label: "Tasks" },
+                { type: _CHECKBOX_, ref: "appointment", label: "Appointments" },
+                { type: _CHECKBOX_, ref: "contact", label: "Contacts" },
+                { type: _TEXTFIELD_, ref: "query", label: "Query", width: "50em" }
+            ]
+        };
+    };
+
+    // Method to create the form manually
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype._createForm = function () {
+
+        // Define the XModel metadata (structure)
+        const xModel = [
+            { id: "message", type: _CHECKBOX_ },
+            { id: "document", type: _CHECKBOX_ },
+            { id: "task", type: _CHECKBOX_ },
+            { id: "appointment", type: _CHECKBOX_ },
+            { id: "contact", type: _CHECKBOX_ },
+            { id: "query", type: _STRING_ }
+        ];
+
+        // Actually create the form using ZaXDialog's supported method
+        this.initForm(xModel, this._xformDef, this._HSMEditObject);
+    };
+
+    // OK button callback function
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype._okCallback = function () {
+        let oldHSMValue = this._HsmPolicyEditContainer.getInstanceValue();
+        var selectedTypes = [];
+        // Collect selected types based on the object values
+        for (let key of ["message", "document", "task", "appointment", "contact"]) {
+            if (this._HSMEditObject[key]) selectedTypes.push(key);
+        }
+
+        // Check if any types are selected and if the query is not empty
+        if (selectedTypes.length === 0 || !this._HSMEditObject.query.trim()) {
+            alert(com_btactic_hsm_admin.MinimumHSMValues);
+            return;
+        }
+
+        // Format the newHSMValue and set the value to the form item
+        let newHSMValue = selectedTypes.join(",") + ":" + this._HSMEditObject.query.trim();
+
+        if (newHSMValue !== oldHSMValue) {
+            this._HsmPolicyEditContainer.setInstanceValue(newHSMValue); // Set the newHSMValue to the form item
+            // simulate the onchange logic: call elementChangedMethod
+            const hsmPolicyEditTextFieldChangedMethod = this._HsmPolicyEditTextField.getElementChangedMethod();
+            hsmPolicyEditTextFieldChangedMethod.call(this._HsmPolicyEditTextField, newHSMValue, oldHSMValue, event||window.event);
+        }
+
+        this.popdown();  // Close the dialog
+    };
+
+    // Cancel button callback function
+    com_btactic_hsm_ext.CustomZaXFormDialog.prototype._cancelCallback = function () {
+        this.popdown();  // Close the dialog
+    };
+
+    // Method to launch the edit wizard dialog
+    com_btactic_hsm_ext.launchEditWizard = function (currentHSMValue, HsmPolicyEditContainer, HsmPolicyEditTextField) {
+        if (typeof currentHSMValue === 'undefined') {
+            currentHSMValue = "";
+        }
+
+        let colonIndex = currentHSMValue.indexOf(":");
+
+        let typesPart = colonIndex !== -1 ? currentHSMValue.slice(0, colonIndex) : currentHSMValue;
+        let queryPart = colonIndex !== -1 ? currentHSMValue.slice(colonIndex + 1) : "";
+
+        let selectedTypes = typesPart ? typesPart.split(",") : [];
+        let query = queryPart;
+
+        // Retrieve the existing DwtShell instance
+        let shell = DwtShell.getShell(window);  // window refers to the current browser window
+
+        if (!(shell instanceof DwtShell)) {
+          throw new Error("shell must be a DwtShell (launchEditWizard)");
+        }
+
+        // Create the dialog instance and pass the shell
+        let dlg = new com_btactic_hsm_ext.CustomZaXFormDialog({
+            parent: shell,      // Pass the DwtShell as the parent
+            className: "DwtDialog",   // You can customize the class name if needed
+            title: "Edit HSM Policy",  // Set the title of the dialog
+            w: "500px",              // Width (can be adjusted)
+            h: "350px",              // Height (can be adjusted)
+            iKeyName: "HSM_POLICY_EDIT",   // Internal key name
+            contextId: Dwt.getNextId(ZaId.DLG_UNDEF),   // Context ID
+            standardButtons: [DwtDialog.OK_BUTTON, DwtDialog.CANCEL_BUTTON]  // Specify standard buttons
+        });
+        // Set the object to be edited
+        dlg.setHSMEditObject({
+            message: selectedTypes.includes("message"),
+            document: selectedTypes.includes("document"),
+            task: selectedTypes.includes("task"),
+            appointment: selectedTypes.includes("appointment"),
+            contact: selectedTypes.includes("contact"),
+            query: query
+        });
+        // Define the form content structure
+        dlg.setContent({
+            type: _GROUP_,
+            numCols: 1,
+            items: [
+                { type: _CHECKBOX_, ref: "message", label: com_btactic_hsm_admin.EmailsType },
+                { type: _CHECKBOX_, ref: "document", label: com_btactic_hsm_admin.DocumentsType },
+                { type: _CHECKBOX_, ref: "task", label: com_btactic_hsm_admin.TasksType },
+                { type: _CHECKBOX_, ref: "appointment", label: com_btactic_hsm_admin.AppointmentsType },
+                { type: _CHECKBOX_, ref: "contact", label: com_btactic_hsm_admin.ContactsType },
+                { type: _TEXTFIELD_, ref: "query", label: com_btactic_hsm_admin.Query, width: "50em" }
+            ]
+        });
+        dlg._createForm();
+        // Register the callback functions for OK and Cancel
+        dlg.registerCallback(DwtDialog.OK_BUTTON, dlg._okCallback.bind(dlg));
+        dlg.registerCallback(DwtDialog.CANCEL_BUTTON, dlg._cancelCallback.bind(dlg));
+
+        // Open the dialog
+        dlg._HsmPolicyEditContainer = HsmPolicyEditContainer;
+        dlg._HsmPolicyEditTextField = HsmPolicyEditTextField;
+        dlg.popup();
+    };
+
 
 }
