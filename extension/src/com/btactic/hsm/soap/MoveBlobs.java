1126aa0442e61f8bac3c18cbe27f219d92ad2185
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-08-02 12:07:28 +0200

    MoveBlobs: Remove draft code.

--- extension/src/com/btactic/hsm/soap/MoveBlobs.java
+++ extension/src/com/btactic/hsm/soap/MoveBlobs.java
@@ -152,27 +152,6 @@ public class MoveBlobs extends AdminDocumentHandler {
             }
         }
 
-        // TODO: Manage loops based on maxBytes being null (no maximum value) or not (with maximum value)
-        // TODO: Move some blobs
-
-        boolean moveNextBlob = true;
-        Long currentMaximumBytes = Long.valueOf(0);
-        Long blobBytes;
-        blobBytes = Long.valueOf(0);
-
-        while (moveNextBlob) {
-            if (!(maxBytes == null)) {
-                // TODO: Get blob size and save it in blobBytes (if any)
-                Long expectedMaximumBytes = currentMaximumBytes + blobBytes;
-                if (expectedMaximumBytes > maxBytes) {
-                    moveNextBlob = false;
-                } else {
-                    currentMaximumBytes = currentMaximumBytes + blobBytes;
-                }
-            }
-            // TODO: Move the blob
-        }
-
         Integer numBlobsMovedMockup = Integer.valueOf(1);
         resp.setNumBlobsMoved(numBlobsMovedMockup);
         Long numBytesMovedMockup = Long.valueOf(2);
