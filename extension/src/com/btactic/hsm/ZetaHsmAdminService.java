53ba05283d74030e5179be4b46af5e0b9a6c9225
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-08-13 11:17:36 +0200

    Added GetScheduleSMPolicy admin service.

--- extension/src/com/btactic/hsm/ZetaHsmAdminService.java
+++ extension/src/com/btactic/hsm/ZetaHsmAdminService.java
@@ -21,6 +21,7 @@ import com.zimbra.cs.service.admin.AdminService;
 
 import com.btactic.hsm.service.admin.AbortHsm;
 import com.btactic.hsm.service.admin.GetHsmStatus;
+import com.btactic.hsm.service.admin.GetScheduleSMPolicy;
 import com.btactic.hsm.service.admin.Hsm;
 import com.btactic.hsm.service.admin.MoveBlobs;
 import com.btactic.hsm.service.admin.ScheduleSMPolicy;
@@ -30,6 +31,7 @@ public class ZetaHsmAdminService extends AdminService implements DocumentService
     public void registerHandlers(DocumentDispatcher dispatcher) {
         dispatcher.registerHandler(HsmConstants.ABORT_HSM_REQUEST, new AbortHsm());
         dispatcher.registerHandler(HsmConstants.GET_HSM_STATUS_REQUEST, new GetHsmStatus());
+        dispatcher.registerHandler(HsmConstants.GET_SCHEDULE_SM_POLICY_REQUEST, new GetScheduleSMPolicy());
         dispatcher.registerHandler(HsmConstants.HSM_REQUEST, new Hsm());
         dispatcher.registerHandler(HsmConstants.MOVE_BLOBS_REQUEST, new MoveBlobs());
         dispatcher.registerHandler(HsmConstants.SCHEDULE_SMPOLICY_REQUEST, new ScheduleSMPolicy());
