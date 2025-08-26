f2329dad27d17991d01b771a28a4907d46f66558
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-08-26 20:31:24 +0200

    ZetaHsmUtil: New approach (11).

--- extension/src/com/btactic/hsm/service/admin/ScheduleSMPolicy.java
+++ extension/src/com/btactic/hsm/service/admin/ScheduleSMPolicy.java
@@ -54,8 +54,8 @@ public final class ScheduleSMPolicy extends AdminDocumentHandler {
         ScheduleSMPolicyRequest req = JaxbUtil.elementToJaxb(request);
 
         Name server = req.getServer();
-        boolean smSchedulePolicyEnabled = request.getOptionalAttributeBool(HsmConstants.A_SM_SCHEDULE_POLICY_ENABLED, false);
-        int smSchedulePolicyStartTime = (int) request.getOptionalAttributeLong(HsmConstants.A_SM_SCHEDULE_POLICY_START_TIME, 0);
+        boolean smSchedulePolicyEnabled = Boolean.parseBoolean(request.getOptionalAttribute(HsmConstants.A_SM_SCHEDULE_POLICY_ENABLED, false));
+        int smSchedulePolicyStartTime = Integer.parseInt(request.getOptionalAttribute(HsmConstants.A_SM_SCHEDULE_POLICY_START_TIME, 0));
 
         checkRight(zsc, context, null, AdminRight.PR_SYSTEM_ADMIN_ONLY);
 
