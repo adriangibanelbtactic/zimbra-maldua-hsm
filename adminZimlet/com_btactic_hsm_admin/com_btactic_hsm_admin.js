102dc648e40163b559d032e33ed2a81030267f1c
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-09-03 19:55:02 +0200

    zimletAdmin: Prepare layout for new buttons.

--- adminZimlet/com_btactic_hsm_admin/com_btactic_hsm_admin.js
+++ adminZimlet/com_btactic_hsm_admin/com_btactic_hsm_admin.js
@@ -226,8 +226,14 @@ if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_hsm_admin"]){
                             {
                                 type: _GROUP_,
                                 width: "100%",
-                                colSizes: ["20%","20%", "60%"]
+                                colSizes: ["20%","20%", "60%"],
                                 items: [
+                                    // HSM Schedule
+                                    {
+                                        type: _ZALEFT_GROUPER_,
+                                        width: "100%",
+                                        label: "HSM Schedule",
+                                    },
                                     // HSM Status Block
                                     {
                                         type: _ZALEFT_GROUPER_,
